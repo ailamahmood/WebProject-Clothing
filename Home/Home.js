b37e010e1e7f185e413f7c405bcdc9b0e349ff81
Aila-Mahmood <ailamahmood2556@gmail.com>
2024-03-31 22:25:51 +0500

    About and Contact Page

--- Home/Home.js
+++ Home/Home.js
@@ -1,7 +1,23 @@
-$(document).ready(function(){
-    $("p").hide(); // Initially hide the paragraph
+  //Toggle Welcome
+  $(document).ready(function(){
+    $("p").hide();                 // Initially para hide
   
     $(".welcome button").click(function(){
       $("p").toggle();
     });
   });
+
+  //Get Email 
+  $(document).ready(function() {
+    $("#emailButton").click(function() {
+        var emailAddress = $(".footer-i.f3 input[type='email']").val();    // Get the email address
+
+        if (emailAddress.includes("@")) {
+          alert("Email address: " + emailAddress + " has been submitted!");
+      } else {
+          alert("Invalid email address! Please enter a valid email address.");
+      }
+
+        $(".footer-i.f3 input[type='text']").val('');       // Clear after submission
+    });
+});
